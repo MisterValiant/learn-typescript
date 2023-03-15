6f8e3d8a81515f7d6428f3e609dc38b5f38f3ae2
MisterValiant <81043907+MisterValiant@users.noreply.github.com>
2023-03-15 21:26:15 +0400

    basics ts
    variables, function, arrays and tuples

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let course = 'typescript';
+let is_published = true;
+let sales = 123456789;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+numbers[0] = 2;
+let user = [1, "Sarv"];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-let age: number = 20
-if (age < 50)
-    age += 10;
-    console.log(age);
+// with anotations
+let course: string = 'typescript';
+let is_published: boolean = true;
+
+// let sales: number=123_456_789;
+// without annotations
+let sales = 123_456_789;
+
+// no initialisation (type any)
+// Best practice to avoid 'any', else what the use of ts over js?
+let level;
+
+// May use "any type" in this context where the type is unknown
+function render(document:any) {
+    console.log(document);
+}
+
+// Declaring arrays
+let numbers:number[] = [1, 2, 3];
+numbers[0]=2;
+
+// Fixed length array
+let user: [number, string] = [1, "Sarv"]
