6c7025a0ec3251bc439a6189736103b7844d58c4
MisterValiant <81043907+MisterValiant@users.noreply.github.com>
2023-03-15 21:37:11 +0400

    constants and enums

--- dist/index.js
+++ dist/index.js
@@ -9,4 +9,11 @@ function render(document) {
 let numbers = [1, 2, 3];
 numbers[0] = 2;
 let user = [1, "Sarv"];
+const small = 1;
+const medium = 2;
+const large = 3;
+;
+let mySize = 1;
+console.log(mySize);
+console.log('Done');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,27 @@ let sales = 123_456_789;
 let level;
 
 // May use "any type" in this context where the type is unknown
-function render(document:any) {
+function render(document: any) {
     console.log(document);
 }
 
 // Declaring arrays
-let numbers:number[] = [1, 2, 3];
-numbers[0]=2;
+let numbers: number[] = [1, 2, 3];
+numbers[0] = 2;
 
 // Fixed length array
 let user: [number, string] = [1, "Sarv"]
+
+// using enums to represent a group of related variables / constants
+
+const small = 1;
+const medium = 2;
+const large = 3;
+
+const enum Size { Small = 0, Medium, Large };
+// enum automatically add the numbers starting from initial number
+
+let mySize: Size =Size.Medium
+
+console.log(mySize)
+console.log('Done')
