747924e7c736d82b3910dde75ad2d0bb0408937e
MisterValiant <81043907+MisterValiant@users.noreply.github.com>
2023-03-15 21:53:33 +0400

    working with functions

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,39 @@ const large = 3;
 const enum Size { Small = 0, Medium, Large };
 // enum automatically add the numbers starting from initial number
 
-let mySize: Size =Size.Medium
+let mySize: Size = Size.Medium
 
 console.log(mySize)
 console.log('Done')
+
+// Functions
+function calculateTax(income: number) {
+    return 0;
+}
+
+// Function + return no values
+function calculateTTax(income: number): void {
+
+}
+
+// Function + return annotation
+function calculateTTTax(income: number, taxYear:number): number {
+    let x;
+    if (taxYear<2022)
+        return income * 1.2;
+    return income * 1.3; //else   
+}
+
+calculateTTTax(10_000, 2022);
+
+// optional parameter
+function calculateTTTTax(income: number, taxYear=2022): number {
+    if (taxYear<2022)
+        return income * 1.2;
+    return income * 1.3; //else   
+}
+
+calculateTTTTax(10_000);
+
+// Always properly annotate functions, parameters and return types
+// enable noUnusedlocals, noUnsedParametes, noImplicitReturns in config
